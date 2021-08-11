fd09e43225f95c98544c85a7fceeb5403248a5bc
Elnaz <e.pouranbarani@gmail.com>
2021-08-11 14:49:18 -0400

    Created and exported unique_titles and retiring_titles tables (used distinct on,count, group by, and order by)

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -200,3 +200,20 @@ INNER JOIN titles as ti
 ON (e.emp_no=ti.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no
+
+SELECT DISTINCT ON (rt.emp_no)
+       rt.emp_no,
+       rt.first_name,
+	   rt.last_name,
+	   rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no, to_date DESC
+
+SELECT count(ut.title), 
+             ut.title
+INTO retiring_titles 
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count(ut.title) DESC
+
